5972dde3e0db1b4ea3ec5f5e28868a8bf8a0c90c
Silvina Delgado <silvinadelgadotridon@gmail.com>
2023-10-14 13:42:43 -0300

    Comienzo  MVC para noticias y agrego contenido a templates

--- app/controller/news.controller.php
+++ app/controller/news.controller.php
@@ -2,3 +2,48 @@
 
    require_once('app/view/news.view.php');
    require_once('app/model/news.model.php');  
+
+   class NewsController {
+      private $model;
+      private $view;
+  
+      public function __construct() {  
+          $this->model = new NewsModel();
+          $this->view = new NewsView();
+      }
+  
+      public function showNews() {
+          $newss = $this->model->getNews();
+          $this->view->showNews($newss);
+      }
+  
+      public function addNews() {
+  
+          $title = $_POST['title'];
+          $content = $_POST['content'];
+          $date = $_POST['date'];
+          $hour = $_POST['hour'];
+          $id_section = $_POST['id_section'];
+         
+          if (empty($title) || empty($content)|| empty($date)|| empty($hour)|| empty($id_section)) {
+              $this->view->showError("Debe completar todos los campos");
+              return;
+          }
+  
+          $id = $this->model->insertNews($title, $content, $date, $hour, $id_section);
+          if ($id) {
+              header('Location: ' . BASE_URL);
+          } else {
+              $this->view->showError("Error al insertar la noticia");
+          }
+      }
+  
+      function removeNews($id) {
+          $this->model->deleteNews($id);
+          header('Location: ' . BASE_URL);
+      }
+      
+      
+  
+  }
+  
--- router.php
+++ router.php
@@ -1,9 +1,10 @@
 <?php
-require_once('app/controller/news.controller.php');
+require_once('./app/controller/news.controller.php');
+
 
 define('BASE_URL', '//'.$_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']).'/');
 
-$action = 'listar'; // accion por defecto
+$action = 'listar'; 
 if (!empty( $_GET['action'])) {
     $action = $_GET['action'];
 }
@@ -14,13 +15,27 @@ $params = explode('/', $action);
 
 switch ($params[0]) {
     case 'listar':
+        $controller = new NewsController();
+        $controller->showNews();
         break;
+
     case 'agregar':
+        $controller = new NewsController();
+        $controller->addNews();
         break;
     case 'eliminar':
+        $controller = new NewsController();
+        $controller->removeNews($params[1]);
         break;
     case 'editar':
+        $controller = new NewsController();
+        //habria que agregar unna funcion editar
         break;
+    case 'finalizar':
+        $controller = new NewsController();
+        $controller->finishNews($params[1]);
+        break;
+
     //case 'login':
     //    break;
     //case 'auth':
@@ -28,6 +43,6 @@ switch ($params[0]) {
     //case 'logout':
     //    break;
     default: 
-        //echo "404 Page Not Found";
+        echo "404 Page Not Found";
         break;
 }
