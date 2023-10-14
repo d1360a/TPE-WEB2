8e9575baf6fb38dc931b319916966935b541a10b
Diego Diaz <sauldiazdiegodiaz@gmail.com>
2023-10-14 15:14:55 -0300

    Se corrigieron errores, en login se mejoro el aspecto visual, se agrego la base de datos actualizada con user y pass

--- router.php
+++ router.php
@@ -1,5 +1,6 @@
 <?php
 require_once('./app/controller/news.controller.php');
+require_once('./app/controller/auth.controller.php');
 
 
 define('BASE_URL', '//'.$_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']).'/');
